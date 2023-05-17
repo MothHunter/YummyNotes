e83d74a8fbc6eab443a2615396d02128d09542e7
Florian Bodner <florian.bodner@stud.fh-campuswien.ac.at>
2023-05-17 12:11:40 +0200

    RecipeScreen

--- app/src/main/java/com/example/yummynotes/screens/RecipeScreen.kt
+++ app/src/main/java/com/example/yummynotes/screens/RecipeScreen.kt
@@ -1,10 +1,11 @@
 package com.example.yummynotes.screens
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.gestures.rememberScrollableState
+import androidx.compose.foundation.gestures.scrollable
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -23,33 +24,42 @@ import com.example.yummynotes.widgets.TopNavigationBar
 
 @Composable
 fun RecipeScreen(navController: NavController, viewModel: RecipeViewModel, recipeID: Int) {
-    val recipe = viewModel.getRecipeByID(recipeID)
 
+    val recipe = viewModel.getRecipeByID(recipeID)
 
     Column(modifier = Modifier.fillMaxWidth(),
     verticalArrangement = Arrangement.spacedBy(5.dp)){
         TopNavigationBar(recipe.title, navController)
-        Text(text =recipe.title,
-        fontSize = 40.sp,
-        textAlign = TextAlign.Center,
-        modifier = Modifier.fillMaxWidth())
-        Image(painter = painterResource(id = recipe.images[0]),
-            contentDescription = "Bild von ${recipe.title}",
-        contentScale = ContentScale.FillWidth)
-        Text(text = "Beschreibung",
-            fontSize = 30.sp,
-            textAlign = TextAlign.Center,
-            modifier = Modifier.fillMaxWidth())
-        Text(text =recipe.description,
-            fontSize = 20.sp,
-            modifier = Modifier.fillMaxWidth())
-        Text(text = "Zutaten",
-            fontSize = 30.sp,
-            textAlign = TextAlign.Center,
-            modifier = Modifier.fillMaxWidth())
-        Text(text = recipe.ingredients.replace(", ", "\n"),
-            fontSize = 20.sp,
-            modifier = Modifier.fillMaxWidth())
+        Box {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .verticalScroll(rememberScrollState()),
+            ) {
+                Text(text =recipe.title,
+                    fontSize = 40.sp,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.fillMaxWidth())
+                Image(painter = painterResource(id = recipe.images[0]),
+                    contentDescription = "Bild von ${recipe.title}",
+                    contentScale = ContentScale.Crop,
+                            modifier = Modifier.fillMaxWidth()
+                                .height(210.dp))
+                Text(text = "Beschreibung",
+                    fontSize = 30.sp,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.fillMaxWidth())
+                Text(text =recipe.description,
+                    fontSize = 20.sp,
+                    modifier = Modifier.fillMaxWidth())
+                Text(text = "Zutaten",
+                    fontSize = 30.sp,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.fillMaxWidth())
+                Text(text = recipe.ingredients.replace(", ", "\n"),
+                    fontSize = 20.sp,
+                    modifier = Modifier.fillMaxWidth())
+            }
+        }
     }
-
 }
