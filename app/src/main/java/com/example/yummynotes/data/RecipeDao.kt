cd174b6434bc03fc6334f95cbbdac271b22f4d7b
Florian Bodner <florian.bodner@stud.fh-campuswien.ac.at>
2023-05-22 16:49:33 +0200

    should have been in working branch

---
+++ app/src/main/java/com/example/yummynotes/data/RecipeDao.kt
@@ -0,0 +1,20 @@
+package com.example.yummynotes.data
+
+import androidx.room.*
+import com.example.yummynotes.models.Recipe
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface RecipeDao {
+    @Insert
+    suspend fun add(recipe: Recipe)
+
+    @Update
+    suspend fun update(recipe: Recipe)
+
+    @Delete
+    suspend fun delete(recipe: Recipe)
+
+    @Query("SELECT * FROM recipe")
+    fun getAllRecipes(): Flow<List<Recipe>>
+}
