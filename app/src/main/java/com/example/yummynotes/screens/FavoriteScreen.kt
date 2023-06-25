5afc93b129015801a74279bd390a9681888335c8
Florian Bodner <florian.bodner@stud.fh-campuswien.ac.at>
2023-06-26 00:03:10 +0200

    some fixes

--- app/src/main/java/com/example/yummynotes/screens/FavoriteScreen.kt
+++ app/src/main/java/com/example/yummynotes/screens/FavoriteScreen.kt
@@ -3,7 +3,11 @@ package com.example.yummynotes.screens
 //import androidx.compose.runtime.collectAsState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -15,45 +19,53 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.example.yummynotes.RecipeList
 import com.example.yummynotes.RecipeRow
+import com.example.yummynotes.data.RecipeDatabase
 import com.example.yummynotes.models.FavoriteViewModel
+import com.example.yummynotes.models.FavoriteViewModelFactory
 import com.example.yummynotes.models.Recipe
+import com.example.yummynotes.models.RecipeScreenViewModelFactory
+import com.example.yummynotes.navigation.Screen
 import com.example.yummynotes.repository.RecipeRepository
 import com.example.yummynotes.utils.Injector
+import com.example.yummynotes.widgets.SimpleTopAppBar
 import com.example.yummynotes.widgets.TopNavigationBar
+import kotlinx.coroutines.launch
 
 @Composable
-fun FavoriteScreen(navController: NavController){
+fun FavoriteScreen(navController: NavController) {
     val viewModel: FavoriteViewModel = viewModel(factory = Injector.provideFavoriteViewModelFactory(
         LocalContext.current))
-
-    val recipesState by viewModel.recipes.collectAsState()
+    val recipeList by viewModel.recipes.collectAsState()
     val coroutineScope = rememberCoroutineScope()
 
-    Column{
-        TopNavigationBar("Lieblingsrezepte", navController)
-        //RecipeList(viewModel.recipes, navController)
-    }
-/*
-    @Composable
-    fun FavoriteRow(recipes: List<Recipe>,
-                    navController: NavController) {
-        LazyColumn(modifier = Modifier.background(color = Color.LightGray)) {
-            recipes.forEach { recipe ->
-                RecipeCard(
-                    recipe = recipe,
-                    onRecipeClick = { recipeID ->
-                        navController.navigate(Screen.RecipeScreen.withId(recipeID))
-                    },
-                    onFavClick = { recipeID ->
-                        // Handle favorite click event
-                        coroutineScope.launch {
-                            favoriteViewModel.updateFavoriteRecipes(recipe)
+    Scaffold(topBar = {
+        SimpleTopAppBar(title = "Lieblingsrezepte", arrowBackClicked = { navController.popBackStack() })
+        {
+            Text(text = "Meine Lieblingsrezepte")
+
+        }
+    }) { //content padding not used
+            padding ->
+        Column(modifier = Modifier.padding(padding)) {
+            LazyColumn {
+                items(items = recipeList) { recipeItem ->
+                    RecipeRow(
+                        recipe = recipeItem,
+                        onRecipeClick = { recipeID: Int ->
+                            navController.navigate(Screen.RecipeScreen.withId(recipeID))
+                        },
+                        onFavIconClick = { recipe ->
+                            coroutineScope.launch {
+                                viewModel.updateFavorites(recipe)
+                            }
                         }
-                    }
-                )
+
+                    )
+                }
+
             }
+
+
         }
     }
-
-
-    }*/}
+}
